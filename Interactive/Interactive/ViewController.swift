4ed6aae9ff419ee6e9a25f4321ee3b32c6c91e78
Vlad <snawyx@gmail.com>
2023-01-05 17:31:39 +0300

    update

--- Interactive/Interactive/ViewController.swift
+++ Interactive/Interactive/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  Interactive
-//
-//  Created by S2 on 5.01.23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
