f158e1a23b38d18f3c7461e857b486db0e3deac7
Vlad <snawyx@gmail.com>
2023-01-05 17:22:13 +0300

    add proj

---
+++ Interactive/Interactive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Interactive
+//
+//  Created by S2 on 5.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
